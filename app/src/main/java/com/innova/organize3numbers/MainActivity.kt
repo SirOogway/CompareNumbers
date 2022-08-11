224628dba3a6208cef8df4a2ef860776ed9ca123
ChonchitoFeliz <jesusandresrodriguezgarcia@outlook.com>
2022-08-10 23:29:33 -0500

    first Version stable and complete

--- app/src/main/java/com/innova/organize3numbers/MainActivity.kt
+++ app/src/main/java/com/innova/organize3numbers/MainActivity.kt
@@ -5,29 +5,37 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
-
-    var etNumberOne: EditText = findViewById(R.id.etNumberOne)
-    var etNumberTwo: EditText = findViewById(R.id.etNumberTwo)
-    var etNumberThree: EditText = findViewById(R.id.etNumberThree)
-    val btnOrganize: Button = findViewById(R.id.btnOrganize)
-    var tvResult: TextView = findViewById(R.id.tvResult)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOrganize.setOnClickListener {
-            val a = etNumberOne.text.toString().toDouble()
-            val b = etNumberTwo.text.toString().toDouble()
-            val c = etNumberThree.text.toString().toDouble()
-            val numberList = listOf(a,b,c).sorted()
+        var etNumberOne: EditText = findViewById(R.id.etNumberOne)
+        var etNumberTwo: EditText = findViewById(R.id.etNumberTwo)
+        var etNumberThree: EditText = findViewById(R.id.etNumberThree)
+        val btnOrganize: Button = findViewById(R.id.btnOrganize)
+        var tvResult: TextView = findViewById(R.id.tvResult)
 
-            tvResult.text = """
+        btnOrganize.setOnClickListener {
+            try {
+                val a = etNumberOne.text.toString().toDouble()
+                val b = etNumberTwo.text.toString().toDouble()
+                val c = etNumberThree.text.toString().toDouble()
+
+                val numberList = listOf(a, b, c).sorted()
+
+                tvResult.text = """
                 minor number: ${numberList[0]}
                 middle number: ${numberList[1]}
                 major number: ${numberList[2]}
             """.trimIndent()
+
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "Ingrese todos los campos", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
