4778b6bbafa583271c8baf8b6d181b415dc46a6e
ChonchitoFeliz <jesusandresrodriguezgarcia@outlook.com>
2022-08-13 15:40:24 -0500

    final version v_0.7.0

--- app/src/main/java/com/innova/comparenumbers/MainActivity.kt
+++ app/src/main/java/com/innova/comparenumbers/MainActivity.kt
@@ -2,38 +2,38 @@ package com.innova.comparenumbers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import com.innova.comparenumbers.R
+import com.innova.comparenumbers.databinding.ActivityMainBinding
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val etNumberOne: EditText = findViewById(R.id.etNumberOne)
-        val etNumberTwo: EditText = findViewById(R.id.etNumberTwo)
-        val etNumberThree: EditText = findViewById(R.id.etNumberThree)
-        val btnOrganize: Button = findViewById(R.id.btnOrganize)
-        val tvResult: TextView = findViewById(R.id.tvResult)
+        binding.apply {
 
-        btnOrganize.setOnClickListener {
-            try {
-                val a = etNumberOne.text.toString().toDouble()
-                val b = etNumberTwo.text.toString().toDouble()
-                val c = etNumberThree.text.toString().toDouble()
+            btnOrganize.setOnClickListener {
+                try {
+                    val a = etNumberOne.text.toString().toDouble()
+                    val b = etNumberTwo.text.toString().toDouble()
+                    val c = etNumberThree.text.toString().toDouble()
 
-                val numberList = listOf(a, b, c)
-                val comparisonSignList = defComparativeSigns(numberList)
+                    val numberList = listOf(a, b, c)
+                    val comparisonSignList = defComparativeSigns(numberList)
 
-                tvResult.text =
-                    "${numberList[0]} ${comparisonSignList[0]} ${numberList[1]} ${comparisonSignList[1]} ${numberList[2]}"
+                    tvResult.text =
+                        "${numberList[0]} ${comparisonSignList[0]} ${numberList[1]} ${comparisonSignList[1]} ${numberList[2]}"
 
-            } catch (e: NumberFormatException) {
-                Toast.makeText(this, "Ingrese todos los campos", Toast.LENGTH_SHORT).show()
+                } catch (e: NumberFormatException) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Ingrese todos los campos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
